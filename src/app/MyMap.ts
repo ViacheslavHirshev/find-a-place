6ef5c34016b83372c2b21ee76373ac73dcf03b2f
Viacheslav Hirshev <viacheslavgirshev@gmail.com>
2025-03-24 18:50:27 +0200

    Logic works fine now

--- src/app/MyMap.ts
+++ src/app/MyMap.ts
@@ -4,32 +4,106 @@ import View from "ol/View"
 import TileLayer from "ol/layer/Tile"
 import OSM from "ol/source/OSM"
 import { fromLonLat } from "ol/proj"
+import Vector from "ol/layer/Vector"
+import VectorSource from "ol/source/Vector"
+import { Feature } from "ol"
+import Point from "ol/geom/Point"
+import Style from "ol/style/Style"
+import Icon from "ol/style/Icon"
+import { Coordinate } from "ol/coordinate"
 
 export class MyMap
 {
     private containerID: string;
     private map: Map;
+    private vectorSource: VectorSource;
 
     constructor(containerID: string)
     {
         this.containerID = containerID;
+        this.vectorSource = new VectorSource();
+
         this.map = new Map(
             {
-                target: `${containerID}`,
+                target: `${this.containerID}`,
                 view: new View(
                     {
                         center: [0, 0],
-                        zoom: 4
+                        zoom: 3
                     }),
                 layers: [
-                    new TileLayer({ source: new OSM() })
+                    new TileLayer({ source: new OSM() }),
+                    new Vector({ source: this.vectorSource })
                 ]
-            }
-        )
+            });
     }
 
-    public changeCoordinates(lon: string, lat: string): void
+    private changeCoordinates(coordinates: Coordinate): void
     {
-        this.map.getView().setCenter(fromLonLat([parseFloat(lon), parseFloat(lat)]));
+        this.map.getView().setCenter(fromLonLat(coordinates));
+        this.map.getView().setZoom(6);
+        this.addMarker(coordinates)
+    }
+
+    private addMarker(coordinates: Coordinate): void
+    {
+        const coords = fromLonLat(coordinates);
+
+        this.vectorSource.clear();
+
+        const marker = new Feature({
+            geometry: new Point(coords)
+        });
+
+        marker.setStyle(new Style({
+            image: new Icon({
+                anchor: [0.5, 1],
+                src: "https://openlayers.org/en/latest/examples/data/icon.png"
+            })
+        }));
+
+        this.vectorSource.addFeature(marker);
+    }
+
+    public showMyLocation(): void
+    {
+        if ("geolocation" in navigator)
+        {
+            navigator.geolocation.getCurrentPosition(
+                (position) =>
+                {
+                    const lon = position.coords.longitude;
+                    const lat = position.coords.latitude;
+                    this.changeCoordinates([lon, lat]);
+                },
+                (error) =>
+                {
+                    console.error("Geolocation error", error);
+                },
+                {
+                    enableHighAccuracy: true
+                });
+        }
+        else
+        {
+            alert("Geolocation is not supported");
+        }
+    }
+
+    public async showLocation(location: string): Promise<void>
+    {
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(location)}`);
+
+        if (response.ok)
+        {
+            const data = await response.json();
+            const lon = parseFloat(data[0].lon);
+            const lat = parseFloat(data[0].lat);
+            this.changeCoordinates([lon, lat]);
+        }
+        else 
+        {
+            console.error("Failed to fetch data");
+        }
     }
 }
--- src/app/main.ts
+++ src/app/main.ts
@@ -1,4 +1,20 @@
 import "../styles/style.css"
-import { MyMap } from "./MyMap"
+import { MyMap } from "./myMap";
 
 const map = new MyMap("map");
+
+const form = document.querySelector(".input-form")! as HTMLFormElement;
+form.addEventListener("submit", (event) =>
+{
+    event.preventDefault();
+
+    const location = (form.querySelector("#user-input") as HTMLInputElement).value;
+    map.showLocation(location);
+});
+
+
+const myGeoposBtn = document.querySelector("#my-geopos-btn")! as HTMLButtonElement;
+myGeoposBtn.addEventListener("click", () =>
+{
+    map.showMyLocation();
+});
